072f369854df6d05118b251d1acd809ec1cdc3fe
SergeiT <uraniumcore238@gmail.com>
2024-03-19 22:29:37 +0200

    level 3

--- level_3/2.py
+++ level_3/2.py
@@ -1,7 +1,12 @@
+from typing import TypeAlias, TypedDict
+
 from constants import ___
 
 
-def calculate_total_spent_for_user(user: ___) -> ___:
+User: TypeAlias = TypedDict[str, str, int, str, list[int]]
+
+
+def calculate_total_spent_for_user(user: User) -> int:
     # попробуй тут воспользовать typing.TypedDict
     pass
 
